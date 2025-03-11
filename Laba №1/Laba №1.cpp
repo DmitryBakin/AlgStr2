ba70812b9fa9a3ae10a338456b43d9aa769bee86
Dmitry Bakin <dima.bakin2017@yandex.ru>
2025-03-11 23:51:14 +0700

    made function fileMerges

--- Laba №1/Laba №1.cpp
+++ Laba №1/Laba №1.cpp
@@ -49,6 +49,59 @@ void split(std::string& filename, std::string& fileSplit1, std::string& fileSpli
 	file_2.close();
 }
 
+void fileMerges(const int p, std::ifstream *inputFile, std::ofstream* outputFile)
+{
+
+	int* mas = new int[2];
+
+	inputFile[0] >> mas[0];
+	inputFile[1] >> mas[1];
+	int n = 0;
+	while (!inputFile[0].eof() && !inputFile[1].eof())
+	{
+		int i = 0, j = 0;
+		while (!inputFile[0].eof() && !inputFile[1].eof() && i < p && j < p)
+		{
+			if (mas[0] < mas[1])
+			{
+				outputFile[n] << mas[0] << " ";
+				inputFile[0] >> mas[0];
+				i++;
+			}
+			else
+			{
+				outputFile[n] << mas[1] << " ";
+				inputFile[1] >> mas[1];
+				j++;
+			}
+		}
+		while (!inputFile[0].eof() && i < p)
+		{
+			outputFile[n] << mas[0] << " ";
+			inputFile[0] >> mas[0];
+			i++;
+		}
+		while (!inputFile[1].eof() && j < p)
+		{
+			outputFile[n] << mas[1] << " ";
+			inputFile[1] >> mas[1];
+			j++;
+		}
+		n = 1 - n;
+	}
+	while (!inputFile[0].eof())
+	{
+		outputFile[n] << mas[0] << " ";
+		inputFile[0] >> mas[0];
+	}
+	while (!inputFile[1].eof())
+	{
+		outputFile[n] << mas[1] << " ";
+		inputFile[1] >> mas[1];
+	}
+	delete[]mas;
+}
+
 
 void fileSort(std::string& filename)
 {
@@ -56,9 +109,47 @@ void fileSort(std::string& filename)
 	std::string file_1 = "file_1";
 	std::string file_2 = "file_2";
 	std::string file_3 = "file_3";
+	
+
 
 	split(filename, file_0, file_1);
+	int p = 1;
 
+	while(file_1.eof())
+	{
+		std::ifstream inputFile[2];
+		std::ofstream outputFile[2];
+
+		inputFile[0].open(file_0);
+		inputFile[1].open(file_1);
+		outputFile[0].open(file_2);
+		outputFile[1].open(file_3);
+
+		fileMerges(p, inputFile, outputFile);
+
+		inputFile[0].close();
+		inputFile[1].close();
+		outputFile[0].close();
+		outputFile[1].close();
+
+		p *= 2;
+
+		inputFile[0].open(file_2);
+		inputFile[1].open(file_3);
+		outputFile[0].open(file_0);
+		outputFile[1].open(file_1);
+
+		fileMerges(p, inputFile, outputFile);
+
+		inputFile[0].close();
+		inputFile[1].close();
+		outputFile[0].close();
+		outputFile[1].close();
+		
+		p *= 2;
+
+		file.open(file_3);
+	} 
 	
 }
 void main()
@@ -70,7 +161,7 @@ void main()
 			
 			fileSort(filename);
 
-			if (isFileContainsSortedArray("random_mas_" + std::to_string(size) + "_" + std::to_string(border) + ".txt"))
+			if (isFileContainsSortedArray("file_0"))
 				std::cout << "OK";
 			else
 				std::cout << "GG\n";
