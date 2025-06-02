869120d3d577108e37a4557011a20a12a0b1c4bc
Dmitry Bakin <dima.bakin2017@yandex.ru>
2025-06-02 23:09:43 +0700

    The "HashTable" class was created. Some methods were implemented. Classes for hash functions were also made.

---
+++ HashTable/HashTable.h
@@ -0,0 +1,227 @@
+#include <math.h>
+#include <iostream>
+#include <vector>
+#include <list>
+#include <utility>
+#include "/O-O-P/List/List.h"
+
+
+class IHashFunction
+{
+public:
+    virtual int hash(const int& key, const int& capacity) const = 0;
+};
+
+class QuadraticHashFunction : public IHashFunction
+{
+public:
+    int hash(const int& key, const int& capacity) const override
+    {
+        int hash0 = key % capacity;
+        int hash = (hash0 + 2) % capacity;
+
+        return hash;
+    }
+};
+
+class MultiplicationHashFunction : public IHashFunction
+{
+public:
+    int hash(const int& key, const int& capacity) const override
+    {
+        int hash0 = key % capacity;
+        double a = -(1. - std::sqrt(5)) / 2.;
+
+        int hash = int(hash0 * a * capacity) % capacity;
+
+        return hash;
+    }
+};
+
+class ThirdHashFunction : public IHashFunction
+{
+public:
+    int hash(const int& key, const int& capacity) const override
+    {
+        int hash0 = key % capacity;
+        int hash = ((hash0 + 1) * (1 + key % (capacity - 2))) % capacity;
+
+        return hash;
+    }
+};
+
+template <typename T>
+class HashTable
+{
+public:
+    HashTable() = default;
+    HashTable(IHashFunction* hashFunction, int capacity);
+    HashTable(const HashTable& other);
+    ~HashTable();
+
+    void setCapacity(int capacity);
+    int capacity() const;
+
+    IHashFunction* hashFunction() const;
+
+    std::vector<List<std::pair<int, T>>> hashTable() const;
+
+    bool contains(const int& key) const;
+    int indexKey(const int& key) const;
+
+    void insert(const int& key, const T& value);
+    void removeKey(const int& key);
+
+    void swap(HashTable& other);
+
+    friend std::ostream& operator<<(std::ostream& os, const HashTable<T>& other);
+
+private:
+    IHashFunction* m_hashFunction = nullptr;
+
+    std::vector<List<std::pair<int, T>>> m_hashTable;
+    int m_capacity = 0;
+
+};
+
+template <typename T>
+HashTable<T>::HashTable(IHashFunction* hashFunction, int capacity)
+    :m_hashFunction(hashFunction), m_capacity(capacity)
+{
+    for (int i = 0; i < m_capacity; i++)
+    {
+        List<std::pair<int, T>> list = List<std::pair<int, T>>();
+        m_hashTable.push_back(list);
+    }
+}
+
+template <typename T>
+HashTable<T>::HashTable(const HashTable& other)
+    :m_capacity(other.capacity()), m_hashFunction(other.hashFunction()), m_hashTable(other.hashTable())
+{
+}
+
+template <typename T>
+HashTable<T>::~HashTable()
+{
+    delete m_hashFunction;
+}
+
+template <typename T>
+void HashTable<T>::setCapacity(int capacity)
+{
+    m_capacity = capacity;
+}
+
+template <typename T>
+int HashTable<T>::capacity() const
+{
+    return m_capacity;
+}
+
+template<typename T>
+IHashFunction* HashTable<T>::hashFunction() const
+{
+    return m_hashFunction;
+}
+
+template <typename T>
+std::vector<List<std::pair<int, T>>> HashTable<T>::hashTable() const
+{
+    return m_hashTable;
+}
+
+template<typename T>
+bool HashTable<T>::contains(const int& key) const
+{
+    int hash = m_hashFunction->hash(key, m_capacity);
+
+    for (int i = 0; i < m_hashTable[hash].size(); i++)
+    {
+        if (m_hashTable[hash][i].first == key)
+        { 
+            return true;
+        }
+    }
+    return false;
+}
+
+template<typename T>
+inline int HashTable<T>::indexKey(const int& key) const
+{
+    int hash = m_hashFunction->hash(key, m_capacity);
+
+    for (int i = 0; i < m_hashTable[hash].size(); i++)
+    {
+        if (m_hashTable[hash][i].first == key)
+        {
+            return i;
+        }
+    }
+    return false;
+}
+
+template <typename T>
+void HashTable<T>::insert(const int& key, const T& value)
+{
+    if (contains(key))
+    {
+        return;
+    }
+    int hash = m_hashFunction->hash(key, m_capacity);
+
+    m_hashTable[hash].addToTail(std::pair<int, T>(key, value));
+}
+
+template<typename T>
+void HashTable<T>::removeKey(const int& key)
+{
+    int index = indexKey(key);
+    if (index == -1)
+    {
+        return;
+    }
+    int hash = m_hashFunction->hash(key, m_capacity);
+
+    m_hashTable[hash].deleteElementFromPosition(index);
+}
+
+template<typename T>
+void HashTable<T>::swap(HashTable& other)
+{
+    std::swap(m_capacity, other.m_capacity);
+    std::swap(m_hashFunction, other.m_hashFunction);
+    std::swap(m_hashTable, other.m_hashTable);
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream& os, HashTable<T>& other)
+{
+    for (int i = 0; i < other.capacity(); i++)
+    {
+        os << "hash = " << i << ":\t";
+        for (int j = 0; j < other.hashTable()[i].size(); j++)
+        {
+            os << "key = " << other.hashTable()[i][j].first << ", value = " << other.hashTable()[i][j].second << " -----> ";
+        }
+        os << "NULL" << std::endl;
+    }
+    return os;
+}
+
+
+
+/*Необходимые методы класса:
+- конструкторы (по умолчанию +, копирования +);
+- деструктор; +
+- добавление элемента с заданным ключом в таблицу; +
+- удаление элемента из таблицы по ключу; +
+- проверка наличия в таблице элемента с заданным ключом; +
+- обмен содержимым с другой таблицей (swap); +
+- вывод содержимого таблицы в консоль (в каждой строке выводить хеш, после этого все связанные с ним пары "ключ-значение"); +
+- вывод содержимого таблицы на форму (Qt);
+- замена хеш-функции (места для уже добавленных элементов должны быть пересчитаны в соответствии с новой функцией);
+- изменение размера хеш-таблицы;
+- оператор присваивания;
+- получение ссылки на значение по ключу (operator []).
+*/
---
+++ HashTable/main.cpp
@@ -0,0 +1,30 @@
+﻿#include "HashTable.h"
+
+int main()
+{
+    HashTable<std::string> HT(new QuadraticHashFunction(), 6);
+
+    HT.insert(23, "1234");
+    HT.insert(65, "1234");
+    HT.insert(75, "1234");
+    HT.insert(2, "1234");
+    HT.insert(8, "1234");
+    HT.insert(5, "1234");
+    HT.insert(9, "1234");
+
+    std::cout << HT;
+
+    HT.removeKey(9);
+
+    std::cout << "\n\n\n" << HT;
+    /*
+    for (int i = 0; i < HT.capacity(); i++)
+    {
+        for (int j = 0; j < HT.hashTable()[i].size(); j++)
+        {
+            std::cout <<"key = " << HT.hashTable()[i][j].first << ", value = " << HT.hashTable()[i][j].second << " -----> ";
+        }
+        std::cout << "NULL" << std::endl;
+    }
+    */
+}
